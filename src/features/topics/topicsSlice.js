5dbcc6802b4a03acee0fba1d42a2db4b202a12d8
KAROLINA_MUELLER <karolina.gawlowska@gmail.com>
2023-03-15 16:15:42 -0400

    create topicsSlice

---
+++ src/features/topics/topicsSlice.js
@@ -0,0 +1,23 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+export const topicsSlice = createSlice({
+    name: "topics",
+    initialState: {
+        topics: {}
+    },
+    reducers: {
+        addTopic: (state, action) => {
+            state.topics[action.payload.id] = {
+                id: action.payload.id,
+                name: action.payload.name,
+                icon: action.payload.icon,
+                quizIds: []
+            };
+        },
+    }
+})
+
+export const selectTopics = (state) => state.topics.topics;
+export const { addTopic } =topicsSlice.actions;
+
+export default topicsSlice.reducer;
